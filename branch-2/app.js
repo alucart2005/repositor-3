2cd8687b741bc27644f2fc4441126f491ec36b5c
alucart2005 <113813382+alucart2005@users.noreply.github.com>
2022-11-21 06:44:18 -0500

    a

---
+++ branch-2/app.js
@@ -0,0 +1,19 @@
+score = 70;
+
+switch (score) {
+  case 90:
+    console.log("A");
+    break;
+  case 80:
+    console.log("B");
+    break;
+  case 70:
+    console.log("C");
+    break;
+  case 60:
+    console.log("D");
+    break;
+  default:
+    console.log("F");
+    break;
+}
