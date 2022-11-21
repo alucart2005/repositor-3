31229c6a9569f480cc7d2a7957c77cd6a37ee99c
alucart2005 <113813382+alucart2005@users.noreply.github.com>
2022-11-21 07:07:33 -0500

    prueba desde vs

--- branch-2/app.js
+++ branch-2/app.js
@@ -1,19 +1,6 @@
 score = 70;
 
-switch (score) {
-  case 90:
-    console.log("A");
-    break;
-  case 80:
-    console.log("B");
-    break;
-  case 70:
-    console.log("C");
-    break;
-  case 60:
-    console.log("D");
-    break;
-  default:
-    console.log("F");
-    break;
-}
+let toScore = score >= 70 ? 'Eres de los buenos' : 'Tiene que volver a estudiar';
+
+document.write("<h1>" + toScore + "</h1>");
+
